9fa463033dedc54f862eba454dd52c4675bbd21a
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-28 19:46:30 +0530

    Add files via upload

---
+++ count_chars.py
@@ -0,0 +1,8 @@
+
+
+def count_characters(s):
+    pass
+
+
+if __name__=="__main__":
+    pass
---
+++ dateandday.py
@@ -0,0 +1,12 @@
+
+def get_month_days(month,year):
+    pass
+
+
+
+def get_week_day(date):
+    pass
+
+
+if __name__=="__main__":
+    pass
